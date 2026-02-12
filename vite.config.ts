d01287e0e2c65d35e75ad52339213f411bf649f3
Rhian Castro <rhi.castro@gmail.com>
2026-02-12 03:19:11 -0300

    fix: update allowedHosts configuration in Vite server and preview settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,12 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     cors: true,
+    allowedHosts: true,
   },
   preview: {
     host: '0.0.0.0',
     port: 3000,
     cors: true,
-    allowedHosts: ['all'],
+    allowedHosts: true,
   },
 })
